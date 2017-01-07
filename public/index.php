0931418db5cf320a340cb7eedde702fb7b5b7527
Sameer Jathavedan <sameerj137@gmail.com>
2017-01-07 12:15:31 +0000

    pset7 half portfolio

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
     // configuration
     require("../includes/config.php"); 
+    
+    $rows = CS50::query("SELECT symbol, shares FROM portfolio WHERE user_id = ?" , $_SESSION["id"]);
 
     // render portfolio
     render("portfolio.php", ["title" => "Portfolio"]);
