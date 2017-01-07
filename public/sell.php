bd5cfcacbfa4b896f3b08881e80f540adad558fa
Sameer Jathavedan <sameerj137@gmail.com>
2017-01-07 18:04:43 +0000

    Done uptil sell

--- public/sell.php
+++ public/sell.php
@@ -6,8 +6,7 @@
     // if user reached page via GET (as by clicking a link or via redirect)
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        // else render form
-        render("sell_form.php", ["title" => "Sell"]);
+        render("sell_form.php", ["title" => "Sell" ]);
     }
 
     // else if user reached page via POST (as by submitting a form via POST)
@@ -18,9 +17,12 @@
             apologize("You must select a stock to sell.");
         }
         
-        $sell = CS50::query("SELECT symbol, shares FROM portfolio WHERE user_id = '?' AND symbol = '?'", $_SESSION["id"], $_POST["sellsym"] );
-        $stock = lookup($sell["symbol"]);
-        CS50::query("DELETE * FROM portfolio WHERE user_id = '?' AND symbol = '?'", $_SESSION["id"], $_POST["sellsym"] );
-        CS50::query("UPDATE users SET cash = cash + '?' WHERE id = '?' " , $sell["shares"]*$stock["price"], $_SESSION["id"]);
+        $sell = CS50::query("SELECT * FROM portfolio WHERE id = ? AND symbol = ?", $_SESSION["id"], $_POST["sellsym"] );
+        $stock = lookup($_POST["sellsym"]);
+        $profit = $sell["shares"] * $stock["price"];
+        CS50::query("DELETE FROM portfolio WHERE id = ? AND symbol = ?", $_SESSION["id"], $_POST["sellsym"] );
+        CS50::query("UPDATE users SET cash = cash + ? WHERE id = ?" , $profit, $_SESSION["id"]);
+        
+        redirect("/");
     } 
 ?>
--- views/sell_form.php
+++ views/sell_form.php
@@ -1,15 +1,11 @@
-<div style="text-align: center">
-    <form action="sell.php" method="post">
-        <?
-         $syms = CS50::query("SELECT DISTINCT symbol FROM `portfolio` WHERE 1");
-        ?>
-        
-        <select name = "sellsym" class="form-control" placeholder="Symbol">
-            <? foreach( $syms as $sym): ?>
-            <option value= "<?= $sym ?>" ><?=$sym?></option>
-            <? endforeach ?>
-        </select>
-        <input class="btn btn-default" type="submit" value="Sell">
-        
-    </form>
-</div>
+<form action="sell.php" method="post">
+  <div class="form-group">
+    <label for="Symbol_input">Symbol:</label>
+    <input type="text" class="form-control" name="sellsym" placeholder="Enter Symbol">
+  </div>
+    <br><br>
+  <div class="form-group">
+    <input class="btn btn-default" type="submit" value="Sell">
+  </div>
+     
+</form>
